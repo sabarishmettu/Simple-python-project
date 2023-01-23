af8ab73191f5152c5e87e8fe42727ef786af0986
Sabarish Mettu <81503474+sabarishmettu@users.noreply.github.com>
2023-01-23 23:47:14 +0530

    Add files via upload

---
+++ caps_to_small.py
@@ -0,0 +1,6 @@
+def convert_to_lowercase(string):
+    return string.lower()
+
+original_string = input("Enter a string in uppercase: ")
+lowercase_string = convert_to_lowercase(original_string)
+print("The lowercase version of the string is:", lowercase_string)
