1981210ad71829b0ab74d693b7e132e2843f403f
Sabarish Mettu <81503474+sabarishmettu@users.noreply.github.com>
2023-03-08 22:40:08 +0530

    Delete Correct Spellings.py

--- Correct Spellings.py
+++
@@ -1,9 +0,0 @@
-from spellchecker import SpellChecker
-corrector = SpellChecker()
-
-word = input("Enter a Word : ")
-if word in corrector:
-    print("Correct")
-else:
-    correct_word = corrector.correction(word)
-    print("Correct Spelling is ", correct_word)
